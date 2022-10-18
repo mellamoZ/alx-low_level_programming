c191268b43dd63c58437e5c32b90e6579ab2517f
mellamoz <zizoxdomi@gmail.com>
2022-10-18 14:03:48 +0300

    doneit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "school.h"
+
+/**
+ *  * this prints _putchar, followed by a new line
+ *    */
+int main(void)
+{
+		_putchar('_');
+			_putchar('p');
+				_putchar('u');
+					_putchar('c');
+						_putchar('h');
+							_putchar('a');
+								_putchar('r');
+									_putchar('\n');
+return (0);
+}
