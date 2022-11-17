536252797aabee8422ccf15fed88e56579e8cd35
mellamoz <zizoxdomi@gmail.com>
2022-11-17 20:01:20 +0300

    done it

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,6 +12,25 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
+
+#include "function_pointers.h"
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
 	}
 
 	return (-1);
