1bdf48abb01b39c5dfde7c629822be9e8d46d87c
mellamoz <zizoxdomi@gmail.com>
2022-11-17 20:07:36 +0300

    done it

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,39 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
+#include <stdio.h>
 #include <stdlib.h>
 
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+int main(int argc, char *argv[])
+{
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
-#endif
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
