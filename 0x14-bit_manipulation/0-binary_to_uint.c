e80c552a9c179c21dbfa39ba4d4dd324fd52b52c
mellamoz <zizoxdomi@gmail.com>
2022-12-01 16:55:35 +0300

    bit manipulation commit 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,23 +2,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int num, i;
 
-	if (b == '\0')
+	if (!b || !*b)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	num = i = 0;
+	while (b[i])
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] > 49)
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		else if (b[i] == 49)
+		{
+			num <<= 1;
+			num += 1;
+		}
+		else
+			num <<= 1;
+		i++;
 	}
-
 	return (num);
 }
