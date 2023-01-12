f7deacd91e0b945a7a0b9f54428a8c4151d28139
mellamoz <zizoxdomi@gmail.com>
2023-01-12 12:21:14 +0300

    11th comit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
+}
